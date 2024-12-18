5a69f8ead72171d563679c9f8a9358261945c415
Kaitxn <yuookii@proton.me>
2024-12-18 09:40:34 +0800

    NvChad

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,4 +1,34 @@
-return{
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+        "3rd/image.nvim",
+        opts = {}
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
+  },
+
+  {
       "nvim-neorg/neorg",
       lazy = false,
       version = "*",
@@ -27,4 +57,5 @@ return{
         vim.wo.foldlevel = 99
         vim.wo.conceallevel = 2
       end,
-    }
+    },
+}
